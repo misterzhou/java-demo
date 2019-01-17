4e8a486260ce960ffa0b507d55f6f641b8d0fe2b
guojing <zhgjgm@gmail.com>
2019-01-17 22:58:46 +0800

    添加设计模式模块

---
+++ demo-design-pattern/src/main/java/org/guojing/demo/designpattern/factory/PizzaStore.java
@@ -0,0 +1,11 @@
+package org.guojing.demo.designpattern.factory;
+
+/**
+ * Created at: 2017-09-27
+ *
+ * @author guojing
+ */
+public class PizzaStore {
+
+
+}
---
+++ demo-design-pattern/src/main/java/org/guojing/demo/designpattern/factory/SimpleFactory.java
@@ -0,0 +1,14 @@
+package org.guojing.demo.designpattern.factory;
+
+/**
+ * Created at: 2017-09-27
+ *
+ * 简单工厂模式
+ *
+ * @author guojing
+ */
+public class SimpleFactory {
+
+
+
+}
